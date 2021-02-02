78af6ece24f08c69f52b2f8ebf164f31096ef402
ManojAcharya5 <60639787+ManojAcharya5@users.noreply.github.com>
2021-02-02 17:21:53 +0530

    Update Program.java

--- Program.java
+++ Program.java
@@ -5,19 +5,20 @@ import java.util.Scanner;
 public class Program {
 	public static void main(String[] args) {
 
-		Bowling2 game = new Bowling2();
+		Bowling game = new Bowling2();
 		Scanner sc = new Scanner(System.in);
 		String input;
 		int points;
 
 		for(int i=0; i<3; i++) {
+			//Taking 3 players details
 			System.out.println("Enter input");
 			input = sc.nextLine();
 			String[] values = input.split(" ");
 			
 			String name = values[0];
 			points = Integer.parseInt(values[1]);
-			
+			//CAlling addPlayer method to store player name and scores/points
 			game.addPlayer(name,points);
 		}
 		game.getWinner();
